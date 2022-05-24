aa8d590f259d81aaf59fc23d37a4fadce9bb83e3
Dennis Ludmann <dennisludmann@gmail.com>
2022-05-24 12:49:54 +0200

    projects added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkComponent } from './work/work.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkComponent
+    WorkComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
